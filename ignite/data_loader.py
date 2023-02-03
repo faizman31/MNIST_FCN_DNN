60b60e5bf7cd640b0614c5b0d7ce70e7fd8edaf0
DJ Shin <faizman31@gmail.com>
2023-02-03 20:19:32 +0900

    added Ignite Code

---
+++ ignite/data_loader.py
@@ -0,0 +1,23 @@
+import torch
+
+from torch.utils.data import Dataset,DataLoader
+
+class MNISTDataset(Dataset):
+    def __init__(self,data,labels,flatten=True):
+        self.data=data
+        self.labels=labels
+        self.flatten=flatten
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self,idx):
+        x = self.data[idx]
+        y = self.labels[idx]
+
+        if self.flatten:
+            x = x.reshape(-1)
+
+        return x,y 
+
+    
