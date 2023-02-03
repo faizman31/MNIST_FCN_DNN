b9abaa83c20d2dec8dfa9778098f300b28ffbca2
DJ Shin <faizman31@gmail.com>
2023-02-03 21:01:49 +0900

    developing data_loader.py

--- ignite/data_loader.py
+++ ignite/data_loader.py
@@ -19,4 +19,54 @@ class MNISTDataset(Dataset):
             x = x.reshape(-1)
 
         return x , y
+
+def load_mnist(is_train=True,flatten=True):
+    from torchvision import datasets,transforms
+
+    dataset = datasets.MNIST(
+        '../data',train=is_train,download=True,
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+        ]),
+    )
+
+    x = dataset.data.float() / 255.
+    y = dataset.targets
+
+    if flatten:
+        x = x.reshape(x.shape[0],-1)
+
+    return x,y
+
+def get_loaders(config):
+    x,y = load_mnist(is_train=True,flatten=False)
+
+    train_cnt = int(x.shape[0] * config.train_ratio)
+    valid_cnt = x.shape[0] - train_cnt
+
+    indices = torch.randperm(x.shape[0])
     
+    train_x,valid_x = torch.index_select(
+        x,
+        dim=0,
+        index=indices
+    ).split([train_cnt,valid_cnt],dim=0)
+
+    train_y,valid_y = torch.index_select(
+        y,
+        dim=0,
+        index=indices
+    ).split([train_cnt,valid_cnt],dim=0)
+
+    train_loader = DataLoader(
+        dataset = MNISTDataset(train_x,train_y,flatten=True),
+        batch_size = config.batch_size,
+        shuffle=True
+    )
+    valid_loader = DataLoader(
+        dataset = MNISTDataset(valid_x,valid_y,flatten=True),
+        batch_size = config.batch_size,
+        shuffle=True
+    )
+
+    test_x,test_y
